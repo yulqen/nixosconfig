f6dc798bc3ebdf4b98d75e235f8209e75b7e7e1c
Matthew Lemon <y@yulqen.org>
2023-01-04 16:47:43 +0000

    wip: refactoring

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
                         {
                           home-manager.useGlobalPkgs = true;
                           home-manager.useUserPackages = true;
-                          home-manager.users.lemon = import ./home.nix;
+                          home-manager.users.lemon = import ./machines/x201/home.nix;
                         }
                       ];
           };
--- users/lemon/default.nix
+++ users/lemon/default.nix
@@ -4,14 +4,12 @@
     ./modules
   ];
 
-    users.users.lemon = {
+  users.users.lemon = {
     isNormalUser = true;
     description = "lemon";
     extraGroups = [ "networkmanager" "wheel" "audio"];
     packages = with pkgs; [];
-    # openssh.authorizedKeys.keys = config.sbruder.pubkeys.trustedKeys;
     initialPassword = "foobar"; # for vm
   };
-
 }
 
